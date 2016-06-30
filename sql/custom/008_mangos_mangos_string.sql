6b78ba0bc4f73570ff8e642eb7bfb48536b10f6b
mierzc <mierzc@email.cz>
2016-06-30 13:26:55 +0200

    doplnenie mangos_string k predchadzajucemu commitu

---
+++ sql/custom/008_mangos_mangos_string.sql
@@ -0,0 +1,23 @@
+/*
+SQLyog Community v12.09 (32 bit)
+MySQL - 5.5.44-0+deb8u1 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `mangos_string` (
+	`entry` mediumint (8),
+	`content_default` text ,
+	`content_loc1` text ,
+	`content_loc2` text ,
+	`content_loc3` text ,
+	`content_loc4` text ,
+	`content_loc5` text ,
+	`content_loc6` text ,
+	`content_loc7` text ,
+	`content_loc8` text 
+); 
+insert into `mangos_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) values('819','You can\'t enter Serpentshrine Cavern until you complete quest The Mark of Vashj.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into `mangos_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) values('820','You can\'t enter Black Temple until you complete quest A Distraction for Akama.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into `mangos_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) values('821','You can\'t enter Tempest Keep: The Eye until you obtain The Tempest Key.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into `mangos_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) values('822','You can\'t enter Hyjal until you complete quest xxx.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
