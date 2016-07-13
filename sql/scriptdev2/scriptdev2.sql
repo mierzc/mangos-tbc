114d2355e2bda103594807a83df89331ab66a8fc
mierzc <mierzc@email.cz>
2016-07-13 21:38:45 +0200

    remove UPDATE ScriptName=""

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -4,11 +4,11 @@
 
 TRUNCATE scripted_areatrigger;
 TRUNCATE scripted_event_id;
-UPDATE creature_template SET ScriptName='';
-UPDATE gameobject_template SET ScriptName='';
-UPDATE item_template SET ScriptName='';
-UPDATE instance_template SET ScriptName='';
-UPDATE world_template SET ScriptName='';
+-- UPDATE creature_template SET ScriptName='';
+-- UPDATE gameobject_template SET ScriptName='';
+-- UPDATE item_template SET ScriptName='';
+-- UPDATE instance_template SET ScriptName='';
+-- UPDATE world_template SET ScriptName='';
 /*  */
 
 /* AREATRIGGER */
